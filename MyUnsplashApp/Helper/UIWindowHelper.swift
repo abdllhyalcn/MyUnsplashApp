794f106d8da055128e942af2f76a81ccce112d57
Abdullah Yalçın <49105577+abdllhyalcn@users.noreply.github.com>
2023-07-12 22:58:27 +0300

    tabbar - added views

---
+++ MyUnsplashApp/Helper/UIWindowHelper.swift
@@ -0,0 +1,31 @@
+//
+//  UIWindowHelper.swift
+//  MyUnsplashApp
+//
+//  Created by Abdullah Yalçın on 12.07.2023.
+//
+
+import Foundation
+
+class UIWindowHelper {
+   /*
+    private func getTabBarController() -> UITabBarController {
+        let tabViewController1 = getNavigationController(DashboardViewController(), UITabBarItem(title: R.string.localizable.tabbarMainPage(), image: R.image.tabMain(), selectedImage: R.image.tabMainSelected()))
+        let tabViewController2 = getNavigationController(CampaignsListViewController(), UITabBarItem(title: R.string.localizable.tabbarCampaigns(), image: R.image.tabCampaigns(), selectedImage: R.image.tabCampaignsSelected()))
+        let tabViewController3 = getNavigationController(TransactionMenuViewController(viewModel: TransactionMenuViewModel(transactionMenu: TransactionMenuEnum.PAYMENT)), UITabBarItem(title: R.string.localizable.tabbarPayments(), image: R.image.tabPayments(), selectedImage: R.image.tabPaymentsSelected()))
+        let tabViewController4 = getNavigationController(TransactionListViewController(), UITabBarItem(title: R.string.localizable.tabbarTransactions(), image: R.image.tabTransactions(), selectedImage: R.image.tabTransactionsSelected()))
+        let tabViewController5 = getNavigationController(ProfileViewController(), UITabBarItem(title: R.string.localizable.tabbarProfile(), image: R.image.tabProfile(), selectedImage: R.image.tabProfileSelected()))
+        
+        let tabBarController = TabBarController()
+        tabBarController.viewControllers = [tabViewController1 , tabViewController2 , tabViewController3, tabViewController4, tabViewController5]
+        
+        return tabBarController
+    }
+    
+    private func getNavigationController(_ viewController: UIViewController, _ tabItem: UITabBarItem) -> UINavigationController {
+        let nav = UINavigationController()
+        nav.viewControllers = [viewController]
+        nav.tabBarItem = tabItem
+        return nav
+    }*/
+}
